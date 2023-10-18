2c2d4a43201a758124bdf0629c7bb990b4476f20
santasly <mogotusylvia@gmail.com>
2023-10-18 08:40:15 +0300

    done

--- calc.js
+++ calc.js
@@ -11,13 +11,13 @@ let output3= document.getElementById("year-output");
 let bounce1= document.getElementById('error1');
 let bounce2= document.getElementById('error2');
 let bounce3= document.getElementById('error3');
-
+ let form = document.querySelector('form')
 let submit_btn = document.getElementById("btn");
 
 day.addEventListener("input", function(){
     
     if (day.value > 31  || day.value <= 0 ){
-        bounce1.textContent=" must be a valid date";
+        bounce1.textContent="not valid ";
         isValid = false;
         return;
     }else{
@@ -29,7 +29,7 @@ day.addEventListener("input", function(){
 month.addEventListener("input", function(){
     
     if (month.value > 12 || month.value <= 0){
-        bounce2.textContent=" must be a valid date";
+        bounce2.textContent=" not valid";
         isValid = false;
         return;
     }else{
@@ -40,7 +40,7 @@ month.addEventListener("input", function(){
 })
 year.addEventListener("input", function(){
     if (year.value > 2023 || year.value <= 0){
-        bounce3.textContent=" must be a valid date";
+        bounce3.textContent="not valid";
         isValid = false;
         return;
     }else{
@@ -49,6 +49,32 @@ year.addEventListener("input", function(){
     }
      
 })
-submit_btn.addEventListener("click" function(){
+// output1.addEventListener()
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const BirthDate= new Date(`${year.value}/${month.value}/${day.value}`);
+     
+    const today = new Date();
+     
+console.log(BirthDate);
+
+   
+    //  let DOB = today.getDay;
+    // let MOB =today.getMonth();
+    // let YOB = today.getFullYear();
+
+    const ageInMilliseconds = today - BirthDate;
     
+    const ageInDays = Math.floor(ageInMilliseconds / (24 * 60 * 60 * 1000));
+    const ageInMonths = Math.floor(ageInDays / 30);
+    const ageInYears = Math.floor(ageInMonths / 12);
+
+    output3.textContent = ageInYears + "  years";
+    output2.textContent = ageInMonths % 12 + "  months";
+    output1.textContent = ageInDays % 30 + "  days";
+   
+
+        
 })
+ 
